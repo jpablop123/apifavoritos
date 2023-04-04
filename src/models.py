b7172be4cf6c5e6c512bfeb1a8a574d2a129e969
metantonio <antonio_martinez88@hotmail.com>
2023-04-04 01:48:03 +0000

    updated

--- src/models.py
+++ src/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     password = db.Column(db.String(250), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     name = db.Column(db.String(120), unique=False, nullable=False)
+    favorite_people = db.relationship('FavoritePeople', backref = 'user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.name
@@ -22,12 +23,39 @@ class User(db.Model):
 
 class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    
+    height = db.Column(db.String, unique=False, nullable=False)
     name = db.Column(db.String(120), unique=False, nullable=False)
-
+    mass = db.Column(db.String(80), unique=False, nullable=False)
+    hair_color = db.Column(db.String(80), unique=False, nullable=False)
+    favorite_people = db.relationship('FavoritePeople', backref= 'people', lazy=True)
+    
     def serialize(self):
         return {
             "id": self.id,        
-            "name": self.name
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color":self.hair_color
             # do not serialize the password, its a security breach
         }
+    
+class FavoritePeople (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "people_id": self.people_id,
+            "user_id": self.user_id,
+            "people_name": People.query.get(self.people_id).serialize()["name"],
+            "user_name": User.query.get(self.user_id).serialize()["name"],
+            "user":User.query.get(self.user_id).serialize(),
+            "people":People.query.get(self.people_id).serialize()
+        }
+
+
+# new_favorite = FavoritePeople(user_id= ..... , )
+# new_favorite.user.serialize()
+# new_favorite.people.serialize()
